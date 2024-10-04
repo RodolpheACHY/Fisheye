5cfac03727c88c70fb8d8ba271094435900f90cc
RodolpheACHY <rod23_reseaux@yahoo.fr>
2024-10-04 17:00:41 +0200

    reponse fetch ok mais prb affichage sur le front

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,11 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
+        const reponse = await fetch("./../../data/photographers.json");
+        let photographers = await reponse.json();
+        //photographers = JSON.parse(photographers);
+
+         /*let photographers = [
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -20,12 +24,15 @@
                 "price": 500,
                 "portrait": "account.png"
             },
-        ]
+        ]*/
         // et bien retourner le tableau photographers seulement une fois récupéré
+        console.log(photographers)
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            //photographers: [...photographers, ...photographers, ...photographers]})
+            photographers})
+            
     }
-
+    
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
