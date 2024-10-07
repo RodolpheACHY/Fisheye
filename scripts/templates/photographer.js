77f6ff4c51dad966a1340664d448315fe5fb58ab
opsonet <luc.leonardi@opsonet.com>
2024-10-07 09:06:58 +0200

    domm css work

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,30 +1,36 @@
 function photographerTemplate(data) {
-    const { name, id, city, country, tagline, price, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        // Création d'une balise dédiée à chaque photographe
-        const article = document.createElement( 'article' );
-        
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const h3 = document.createElement( 'h3' );
-        h3.textContent = `${city}, ${country}`; 
-        const p = document.createElement( 'p' );
-        p.textContent = tagline;
-        const pPrice = document.createElement( 'p' )
-        pPrice.textContent = `${price}€/jour`;
-        article.dataset.id = data.id;
-        console.log(article.dataset.id);
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(p);
-        article.appendChild(pPrice);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    // Création d'une balise dédiée à chaque photographe
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.setAttribute("alt", name);
+    img.setAttribute("src", picture);
+    const aLink = document.createElement("a");
+    aLink.setAttribute("href", "photographer.html?id=" + id);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    const p = document.createElement("p");
+    p.textContent = tagline;
+    const pPrice = document.createElement("p");
+    pPrice.textContent = `${price}€/jour`;
+    article.dataset.id = data.id;
+    article.classList.add("photographer-item");
+    console.log(article.dataset.id);
+
+    aLink.appendChild(img);
+    aLink.appendChild(h2);
+    aLink.appendChild(h3);
+    aLink.appendChild(p);
+    aLink.appendChild(pPrice);
+    article.appendChild(aLink);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
