cdfabf918a882412588aa483e82dffefe695631e
RodolpheACHY <rod23_reseaux@yahoo.fr>
2024-10-07 22:41:31 +0200

    ADD integration of the index page + accessibility ---> ok

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -6,24 +6,34 @@ function photographerTemplate(data) {
     function getUserCardDOM() {
         // Création d'une balise dédiée à chaque photographe
         const article = document.createElement( 'article' );
-        
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("alt", `${name} - page photographe de ${name}`);
+        img.setAttribute("src", picture);
+        const aLink = document.createElement("a");
+        aLink.setAttribute("href", "photographer.html?id=" + id);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        const h3 = document.createElement( 'h3' );
-        h3.textContent = `${city}, ${country}`; 
-        const p = document.createElement( 'p' );
-        p.textContent = tagline;
+        const pCityCountry = document.createElement( 'p' );
+        pCityCountry.textContent = `${city}, ${country}`;
+        pCityCountry.classList.add("pCityCountry");
+        const pTagline = document.createElement( 'p' );
+        pTagline.textContent = tagline;
+        pTagline.classList.add("pTagline");
         const pPrice = document.createElement( 'p' )
         pPrice.textContent = `${price}€/jour`;
+        pPrice.classList.add("pPrice");
         article.dataset.id = data.id;
         console.log(article.dataset.id);
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(p);
-        article.appendChild(pPrice);
+        const imgLogoHeader = document.querySelector(".logo");
+        imgLogoHeader.setAttribute("role", "img");
+        article.classList.add("photographer-item");
+        aLink.appendChild(img);
+        aLink.appendChild(h2);
+        aLink.appendChild(pCityCountry);
+        aLink.appendChild(pTagline);
+        aLink.appendChild(pPrice);
+        article.appendChild(aLink);
         return (article);
     }
     return { name, picture, getUserCardDOM }
