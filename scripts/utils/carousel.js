ad4fcad5554a5eded27919e3be6a54e268fa599d
opsonet <luc.leonardi@opsonet.com>
2024-10-31 10:47:33 +0100

    add todo for carousel

--- scripts/utils/carousel.js
+++ scripts/utils/carousel.js
@@ -14,25 +14,29 @@ const buttons = document.querySelectorAll(".btn");
 //const gridItems = document.querySelectorAll(".grid-item");
 //console.log(gridItems);
 
-
 // Tableau d'images: [0, 1, 2]
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        //console.log(e.target.id);
-        const gridItems = document.querySelectorAll(".grid-item");
-        const calcNextSlide = e.target.id === "next" ? 1 : -1;   // pour savoir si on doit faire un +1 ou un -1
-        const slideActive = document.querySelector(".active");
-        //console.log(gridItems);  // slides retourne un nodelist et pas un tableau
-        //console.log([...slides].indexOf(slideActive)); // permet de destructurer la nodelist slides pour créer un tableau et de savoir à quel index se trouve la classe slideActive active aved indexOff()
-        console.log(gridItems, slideActive); 
-        newIndex = calcNextSlide + [...gridItems].indexOf(slideActive);
-        //console.log(newIndex);
-        if (newIndex < 0) newIndex = [...gridItems].length -1;    // pour aller sur le dernier élément du tableau. Ici qd on est sur -1 et clique en arrière, on arrive sur 2.
-        if (newIndex >= [...gridItems].length) newIndex = 0; // qd on est sur 2 et qd on clique sur droite, on retourne à 0
-        gridItems[newIndex].classList.add("active");
-        slideActive.classList.remove("active");
-    });
+  button.addEventListener("click", (e) => {
+    //console.log(e.target.id);
+    const gridItems = document.querySelectorAll(".grid-item");
+   /**
+    * todo récupérer les objets media à partir de l'attribut data-media
+    */
+    const calcNextSlide = e.target.id === "next" ? 1 : -1; // pour savoir si on doit faire un +1 ou un -1
+    const slideActive = document.querySelector(".active");
+    //console.log(gridItems);  // slides retourne un nodelist et pas un tableau
+    //console.log([...slides].indexOf(slideActive)); // permet de destructurer la nodelist slides pour créer un tableau et de savoir à quel index se trouve la classe slideActive active aved indexOff()
+    console.log(gridItems, slideActive);
+    newIndex = calcNextSlide + [...gridItems].indexOf(slideActive);
+    //console.log(newIndex);
+    if (newIndex < 0) newIndex = [...gridItems].length - 1; // pour aller sur le dernier élément du tableau. Ici qd on est sur -1 et clique en arrière, on arrive sur 2.
+    if (newIndex >= [...gridItems].length) newIndex = 0; // qd on est sur 2 et qd on clique sur droite, on retourne à 0
+    gridItems[newIndex].classList.add("active");
+   
+
+    slideActive.classList.remove("active");
+  });
 });
 
 //});
