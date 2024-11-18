cd68765c51876a6dd4e1669264db91155632f5cf
RodolpheACHY <rod23_reseaux@yahoo.fr>
2024-11-18 23:34:13 +0100

    correction derniers prbs d'accessibiltés + format img lightbox ---> ok

--- scripts/utils/sort.js
+++ scripts/utils/sort.js
@@ -1,14 +1,16 @@
 function sortByPopularity() {
-    const sortSelect = document.getElementById('filter-select');
-    console.log("sortSelect: ", sortSelect);
-    sortSelect.addEventListener("change", function () {
-        const selectedValue = this.value;
-        if (selectedValue === "populary") {
-            const sortedMedias = photographerData.medias.sort((a, b) => b.likes - a.likes);
-            console.log("sortedMedias:" + sortedMedias);
-            const mediaContainer = document.getElementById("media-container");
-            mediaContainer.innerHTML = '';
-            displayData(sortedMedias);
-        }   
-    });
+  const sortSelect = document.getElementById("filter-select");
+  console.log("sortSelect: ", sortSelect);
+  sortSelect.addEventListener("change", function () {
+    const selectedValue = this.value;
+    if (selectedValue === "populary") {
+      const sortedMedias = photographerData.medias.sort(
+        (a, b) => b.likes - a.likes
+      );
+      console.log("sortedMedias:" + sortedMedias);
+      const mediaContainer = document.getElementById("media-container");
+      mediaContainer.innerHTML = "";
+      displayData(sortedMedias);
+    }
+  });
 }
